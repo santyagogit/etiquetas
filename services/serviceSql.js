fcf892e5f51143f56d4cb28c53b574fe348395c9
santyago.git <santyago.lp@gmail.com>
2021-06-25 13:56:12 -0300

    Aligning price on etOferta

---
+++ services/serviceSql.js
@@ -0,0 +1,40 @@
+const sql = require('mssql');
+
+let config = {
+  user: 'sams_node', //default is sa
+  password: 'sa',
+  server: 'localhost', // for local machine
+  database: 'ElDuende', // name of database
+  options: {
+    encrypt: false,
+    enableArithAbort: false,
+  },
+};
+
+sql.on('error', (err) => {
+  // ... error handler
+  console.log('Sql database connection error ', err);
+});
+
+async function getEtiquetas() {
+  return await sql
+    .connect(config)
+    .then((pool) => {
+      return pool.request().query('SELECT TOP 5 * FROM etiquetaTMP');
+      // return (
+      //   pool
+      //     .request()
+      //     .input(`VarPrice`, sql.Int, 10)
+      //     // .output('output_parameter', sql.VarChar(50))
+      //     .execute('pr_Names')
+      // );
+    })
+    .then((result) => {
+      return result.recordset;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+module.exports = getEtiquetas;
